6a6a7c6d4c7d92a791d83f1d22f4f57948c09a0d
DeFlesz <nieflesz@gmail.com>
2023-04-12 13:01:46 +0200

    enable discoverability, bugfixes

--- app/src/main/java/antoni/nawrocki/blutacz/MainActivity.java
+++ app/src/main/java/antoni/nawrocki/blutacz/MainActivity.java
@@ -1,7 +1,5 @@
 package antoni.nawrocki.blutacz;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,11 +13,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +25,6 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 import antoni.nawrocki.blutacz.adapter.BluetoothDeviceAdapter;
 
@@ -36,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 421;
     private static final int REQUEST_ENABLE_DISCOVERABLE = 420;
 //    ImageView availibilityIcon;
+    BluetoothAdapter bluetoothAdapter;
     RecyclerView pairedDevicesRecycler;
     RecyclerView newDevicesRecycler;
     TextView macAdress;
@@ -43,16 +41,23 @@ public class MainActivity extends AppCompatActivity {
     LinearProgressIndicator progressIndicator2;
     MaterialButton scanButton;
     MaterialButton enableDiscoverability;
+    MaterialButton enableBluetooth;
+    RelativeLayout container;
+
+    ArrayList<BluetoothDevice> pairedDevices = new ArrayList<>();
+    BluetoothDeviceAdapter bluetoothDeviceAdapter = new BluetoothDeviceAdapter(pairedDevices, this);
+
     ArrayList<BluetoothDevice> newDevices = new ArrayList<>();
     BluetoothDeviceAdapter newDevicesAdapter = new BluetoothDeviceAdapter(newDevices, this);
 
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver newDevicesReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                checkPermission();
                 if (device.getName() == null) {
                     return;
                 }
@@ -67,18 +72,20 @@ public class MainActivity extends AppCompatActivity {
             }
             if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
                 progressIndicator.show();
+                enableDiscoverability.setEnabled(false);
                 newDevices.clear();
                 Log.i("bt-dev", "discovery started");
             }
             if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 progressIndicator.hide();
+                enableDiscoverability.setEnabled(true);
                 scanButton.setIcon(getDrawable(R.drawable.ic_baseline_refresh_24));
                 Log.i("bt-dev", "discovery finished");
             }
         }
     };
 
-    BroadcastReceiver receiver2 = new BroadcastReceiver() {
+    BroadcastReceiver discoverableStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -87,16 +94,20 @@ public class MainActivity extends AppCompatActivity {
                 int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
 
                 switch (mode) {
+                    // device is in discoverable mode
                     case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
-                        Log.d(TAG, "onReceive: discoverable");
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability enabled.");
                         progressIndicator2.show();
+                        enableDiscoverability.setEnabled(false);
                         break;
+                    // device is not in discoverable mode
                     case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
-                        Log.d(TAG, "onReceive: discoverable");
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability disabled. Able to receive connections.");
                         progressIndicator2.hide();
+                        enableDiscoverability.setEnabled(true);
                         break;
                     case BluetoothAdapter.SCAN_MODE_NONE:
-                        Log.d(TAG, "onReceive: discoverable");
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability disabled. Not able to receive connections.");
                         progressIndicator2.hide();
                         break;
                 }
@@ -104,6 +115,43 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    BroadcastReceiver bluetoothStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
+
+                switch  (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        enableBluetooth.setVisibility(View.VISIBLE);
+                        container.setVisibility(View.GONE);
+//                        pairedDevices.clear();
+//                        newDevices.clear();
+//                        bluetoothDeviceAdapter.notifyDataSetChanged();
+//                        newDevicesAdapter.notifyDataSetChanged();
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        enableBluetooth.setVisibility(View.GONE);
+                        container.setVisibility(View.VISIBLE);
+                        checkPermission();
+//                        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                        pairedDevices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+                        bluetoothDeviceAdapter = new BluetoothDeviceAdapter(pairedDevices, getApplicationContext());
+                        pairedDevicesRecycler.setAdapter(bluetoothDeviceAdapter);
+                        Log.d(TAG, "onReceive: ");
+
+                        bluetoothDeviceAdapter.notifyDataSetChanged();
+                        if (!bluetoothAdapter.isDiscovering()) {
+                            scanButton.performClick();
+                        }
+
+                        break;
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,6 +168,8 @@ public class MainActivity extends AppCompatActivity {
         progressIndicator2 = findViewById(R.id.progressIndicator2);
         scanButton = findViewById(R.id.scanDevices);
         enableDiscoverability = findViewById(R.id.enableDiscoverability);
+        enableBluetooth = findViewById(R.id.bluetoothEnable);
+        container = findViewById(R.id.container_bt);
 
         progressIndicator.hide();
         progressIndicator2.hide();
@@ -130,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothAdapter = bluetoothManager.getAdapter();
 //        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (!bluetoothAdapter.isEnabled()) {
@@ -141,7 +191,10 @@ public class MainActivity extends AppCompatActivity {
 //            availibilityIcon.setBackgroundColor(Color.BLUE);
         }
 
-        ArrayList<BluetoothDevice> pairedDevices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+        enableBluetooth.setOnClickListener(l -> {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        });
 //
 //        if (pairedDevices.size() > 0) {
 //            // There are paired devices. Get the name and address of each paired device.
@@ -153,8 +206,11 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }
 
-        BluetoothDeviceAdapter bluetoothDeviceAdapter = new BluetoothDeviceAdapter(pairedDevices, this);
+        pairedDevices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+        bluetoothDeviceAdapter = new BluetoothDeviceAdapter(pairedDevices, this);
         pairedDevicesRecycler.setAdapter(bluetoothDeviceAdapter);
+//        bluetoothDeviceAdapter.notifyDataSetChanged();
+
 
         newDevicesRecycler.setAdapter(newDevicesAdapter);
 
@@ -162,16 +218,19 @@ public class MainActivity extends AppCompatActivity {
 
 //        Log.v("bt-dev", bluetoothAdapter.getState() + " " + bluetoothAdapter.getScanMode());
 
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        registerReceiver(receiver, filter);
+        IntentFilter newDevicesFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        newDevicesFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        newDevicesFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(newDevicesReceiver, newDevicesFilter);
 
-        IntentFilter filter2 = new IntentFilter();
-        filter2.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter2.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        filter2.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-        registerReceiver(receiver2, filter2);
+        IntentFilter discoverableStateFilter = new IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+//        discoverableStateFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+//        discoverableStateFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(discoverableStateReceiver, discoverableStateFilter);
+
+        IntentFilter bluetoothStateFilter = new IntentFilter();
+        bluetoothStateFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(bluetoothStateReceiver, bluetoothStateFilter);
 
         scanButton.setOnClickListener(l -> {
 //            Log.v("bt-dev", "whatever");
@@ -188,7 +247,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        scanButton.performClick();
+//        scanButton.performClick();
+        if (!bluetoothAdapter.isDiscovering()) {
+            scanButton.performClick();
+        }
 
         enableDiscoverability.setOnClickListener(l -> {
             if (bluetoothAdapter.isDiscovering()) {
@@ -235,10 +297,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        unregisterReceiver(receiver);
-        unregisterReceiver(receiver2);
+        checkPermission();
+        bluetoothAdapter.cancelDiscovery();
+        unregisterReceiver(newDevicesReceiver);
+        unregisterReceiver(discoverableStateReceiver);
+        unregisterReceiver(bluetoothStateReceiver);
     }
 }
